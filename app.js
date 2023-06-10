b9264a3d77ab246ba7a036a55797fd11f5030bb6
Alwyn Bradman <45570784+AlwynBrad@users.noreply.github.com>
2023-06-10 21:11:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.post("/", function (req, res) {
   const url = "https://us21.api.mailchimp.com/3.0/lists/fbc4682210";
 
   // Mailchimp API key
-  const apiKey = "b9e76ea292c987f74890d0783e9eb39f-us21";  //ref: b9e76ea292c987f74890d0783e9eb39f-us21
+  const apiKey = "d80e923d5a739b1899fa0fa485b35c42-us21";  //ref: d80e923d5a739b1899fa0fa485b35c42-us21 -mailchimp
 
   // Mailchimp list ID
   const listId = "fbc4682210";
